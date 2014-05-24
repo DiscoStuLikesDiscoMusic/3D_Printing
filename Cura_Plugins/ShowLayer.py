a01067af1fea8652541a259e3aedceb2e3868cba
Metalcrowe <dominic@von-bergen.ch>
2014-05-24 19:04:38 +0200

    Now detects Cutoff

--- Cura_Plugins/ShowLayer.py
+++ Cura_Plugins/ShowLayer.py
@@ -23,24 +23,32 @@ def getLayerNumber(line, default = None):
 with open(filename, "r") as f:
 	lines = f.readlines()
 
-NumberOfLayer = 0;
-ActualLayer=0;
-NumberOfObjects=0;
+LayerHasLayerCountNumber = 0;
+LayerCount = 0;
+ActualLayer = 0;
 ActualObject=0;
 
 with open(filename, "w") as f:
 	for line in lines:
+		if line.startswith(';LAYER:'):
+			if LayerHasLayerCountNumber == 0:
+				LayerHasLayerCountNumber = 1
+				LayerCount = LayerCount + 1
+		else:
+			LayerHasLayerCountNumber = 0;
+			
+	LayerCount = str(LayerCount - 1)
+	
+	for line in lines:
 		f.write(line)
-		#Get the whole count of layer
-		if line.startswith(';Layer count:'):
-			NumberOfObjects = NumberOfObjects+1
-			NumberOfLayer = getLayerNumber(line,None)
-			NumberOfLayer = str(NumberOfLayer)
 		#print the actual layer
 		if line.startswith(';LAYER:'):
 			ActualLayer = getLayerNumber(line,None)
 			#+1 because the LayerNumber starts with 0
 			ActualLayer = int(ActualLayer)
-			ActualLayer = ActualLayer+1
-			ActualLayer = str(ActualLayer)
-			f.write("M117 Layer " + ActualLayer + " of" + NumberOfLayer + "\n")
+			ActualLayer = ActualLayer + 1
+			if ActualLayer <= int(LayerCount):
+				ActualLayer = str(ActualLayer)
+				f.write("M117 Layer " + ActualLayer + " of " + LayerCount + ".\n")
+			
+		
